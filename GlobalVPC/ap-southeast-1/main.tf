a4b31fa6eda47ea7456bd6395012b4aa4ad620db
kgtoledoc <kgtoledoc19@gmail.com>
2022-01-11 06:34:36 -0500

    Provider removed

--- GlobalVPC/ap-southeast-1/main.tf
+++ GlobalVPC/ap-southeast-1/main.tf
@@ -1,9 +1,5 @@
-provider "aws" {
-  alias ="region"
-}
-
 resource "aws_cloudformation_stack" "demo-vpc" {
   name = "ap-southeast-1"
   template_body = "${file("./us-east-2/demo-vpc-ap-southeast-1.yaml")}"
-  provider = "aws.region"
+  provider = aws.region
 }
