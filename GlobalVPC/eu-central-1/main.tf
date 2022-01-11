2adfb15f0c0265a6b0514dcc61142940a3b7584f
kgtoledoc <kgtoledoc19@gmail.com>
2022-01-11 06:39:26 -0500

    Provider added in each module

--- GlobalVPC/eu-central-1/main.tf
+++ GlobalVPC/eu-central-1/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  alias = "region"
+}
+
 resource "aws_cloudformation_stack" "demo-vpc" {
   name = "eu-central-1"
   template_body = "${file("./us-east-2/demo-vpc-eu-central-1.yaml")}"
--- GlobalVPC/us-east-2/main.tf
+++ GlobalVPC/us-east-2/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  alias = "region"
+}
+
 resource "aws_cloudformation_stack" "demo-vpc" {
   name = "us-east-2"
   template_body = "${file("./us-east-2/demo-vpc-us-east-2.yaml")}"
