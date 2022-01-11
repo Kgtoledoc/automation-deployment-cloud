76498abac7e4427281274929e0ea8d69b7b24a04
kgtoledoc <kgtoledoc19@gmail.com>
2022-01-11 06:41:36 -0500

    Fixing the path for cloudformation stack

--- GlobalVPC/ap-southeast-1/main.tf
+++ GlobalVPC/ap-southeast-1/main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "aws_cloudformation_stack" "demo-vpc" {
   name = "ap-southeast-1"
-  template_body = "${file("./us-east-2/demo-vpc-ap-southeast-1.yaml")}"
+  template_body = "${file("./ap-southeast-1/demo-vpc-ap-southeast-1.yaml")}"
   provider = aws.region
 }
--- GlobalVPC/eu-central-1/main.tf
+++ GlobalVPC/eu-central-1/main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "aws_cloudformation_stack" "demo-vpc" {
   name = "eu-central-1"
-  template_body = "${file("./us-east-2/demo-vpc-eu-central-1.yaml")}"
+  template_body = "${file("./eu-central-1/demo-vpc-eu-central-1.yaml")}"
   provider = aws.region
 }
